07d2e0aed6e7516c2c2dbce30ca5a052a073912f
Dmitry Vlasenko <dmitry_vlasenko@ukr.net>
2020-06-04 21:31:02 +0300

    error correction

--- Homework 3/network/terraform.tf
+++ Homework 3/network/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket                  = "devops-terraform-vlasenko"
-    key                     = "management/terraform.tfstate"
+    key                     = "network/terraform.tfstate"
     region                  = "eu-central-1"
     dynamodb_table          = "devops-terraform-vlasenko"
     shared_credentials_file = "/home/dmitry/Lessons-Hillel/Homework 3/creds"
