d7afa64731b40fb66723e7b2108a5360eea64360
Dmitry Vlasenko <dmitry_vlasenko@ukr.net>
2020-06-08 22:09:52 +0300

    changes aws_ebs_volume

--- Homework 5/lvm/instances/main.tf
+++ Homework 5/lvm/instances/main.tf
@@ -49,27 +49,48 @@ resource "aws_security_group" "ssh_hhtp_https" {
     Name = "allow_ssh_test"
   }
 }
+resource "aws_ebs_volume" "a" {
+  availability_zone = aws_instance.Centos7.availability_zone
+  size              = "1"
+  tags = {
+    Name = "a"
+  }
+}
+resource "aws_ebs_volume" "b" {
+  availability_zone = aws_instance.Centos7.availability_zone
+  size              = "1"
+  tags = {
+    Name = "b"
+  }
+}
+resource "aws_ebs_volume" "c" {
+  availability_zone = aws_instance.Centos7.availability_zone
+  size              = "1"
+  tags = {
+    Name = "c"
+  }
+}
+resource "aws_volume_attachment" "a" {
+  device_name = "/dev/sdb"
+  volume_id   = aws_ebs_volume.a.id
+  instance_id = aws_instance.Centos7.id
+}
+resource "aws_volume_attachment" "b" {
+  device_name = "/dev/sde"
+  volume_id   = aws_ebs_volume.b.id
+  instance_id = aws_instance.Centos7.id
+}
+resource "aws_volume_attachment" "c" {
+  device_name = "/dev/sdf"
+  volume_id   = aws_ebs_volume.c.id
+  instance_id = aws_instance.Centos7.id
+}
 resource "aws_instance" "Centos7" {
   ami                    = data.aws_ami.Centos7.id
   instance_type          = local.workspace["instance_type"]
   user_data              = file("scripts/lvm.sh")
   key_name               = "Frankfurt"
   vpc_security_group_ids = ["${aws_security_group.ssh_hhtp_https.id}"]
-  ebs_block_device {
-    device_name = "sdb"
-    volume_type = "gp2"
-    volume_size = "1"
-  }
-  ebs_block_device {
-    device_name = "sde"
-    volume_type = "gp2"
-    volume_size = "1"
-  }
-  ebs_block_device {
-    device_name = "sdf"
-    volume_type = "gp2"
-    volume_size = "1"
-  }
   tags = {
     Name = "Hello Centos7 ${terraform.workspace}"
   }
